031915395cdfd5a1b924a8466f53f24198549ba4
Navami Subhash <navamisubhash93@gmail.com>
2021-10-15 22:32:43 -0400

    Update app.component.ts

--- TravelCan-App/src/app/app.component.ts
+++ TravelCan-App/src/app/app.component.ts
@@ -9,9 +9,15 @@ export class AppComponent {
     { title: 'TravelBuddy', url: 'travel-buddy', icon: 'airplane' },
     { title: 'Rideshare', url: 'ride-share', icon: 'car' },
     { title: 'Accommodation', url: 'accommodation', icon: 'home' },
+<<<<<<< HEAD
     { title: 'Community', url: '/folder/Archived', icon: 'people' },
     { title: 'Testimonial', url: '/folder/Trash', icon: 'chatbubbles' },
     { title: 'ChatBot', url: '/folder/Spam', icon: 'chatbox-ellipses' }
+=======
+    { title: 'Community', url: 'community', icon: 'people' },
+    { title: 'Testimonial', url: 'testimonial', icon: 'chatbubbles' },
+    { title: 'Chat Bot', url: 'chatbot', icon: 'chatbox-ellipses' },
+>>>>>>> 8c1db91 (Update app.component.ts)
   ];
   public labels = ['Help & FAQ'];
   constructor() {}
