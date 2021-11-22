5099fdba7bde55676a598af9ac2aae4b27607917
Arathy Unni <74705549+arathy-1993@users.noreply.github.com>
2021-11-22 14:31:44 -0500

    Create RidePlanDetails.ts

---
+++ TravelCan-App/src/app/models/RidePlanDetails.ts
@@ -0,0 +1,11 @@
+export class RidePlanDetails{
+    firstName : String;
+    lastName : String;
+    airportName : String;
+    destination : String;
+    exitTerminalNumber : number;
+    numberOfPassengers : number;
+    numberOfLuggages : number;
+    contactNumber : string;
+    timeOfRide :  Date;
+}
